9103d2ee797931c35eaf4f2f4925d83f73f7c0b3
Micah Eckard <lumberjacksoul339@gmail.com>
2024-11-04 20:40:11 -0500

    started front page design

--- fresh-ink-front/src/pages/Home.js
+++ fresh-ink-front/src/pages/Home.js
@@ -1,10 +1,18 @@
+import logo from '../Assets/Images/Hero-logo-transparent.png'
 
-
-export default function Home () {
-    return (
-        <div className="home-page">
-            <h1>Fresh Ink</h1>
-            <h2>The Sickest Tattoo Merch</h2>
-        </div>
-    )
+export default function Home() {
+  return (
+    <div className="home-page">
+      <div className="banner">
+        <img
+          src={logo}
+          className='hero'
+          alt="Fresh Ink logo with a sparrow and traditional tattoo flowers"
+        />
+          <h2>
+            The Sickest Tattoo Merch
+          </h2>
+      </div>
+    </div>
+  )
 }
