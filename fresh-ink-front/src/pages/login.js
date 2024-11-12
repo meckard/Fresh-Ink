6544bcd8d9328ecbbefc092de1f16bbda0567362
Micah Eckard <lumberjacksoul339@gmail.com>
2024-11-11 20:53:32 -0500

    finished login page design

--- fresh-ink-front/src/pages/login.js
+++ fresh-ink-front/src/pages/login.js
@@ -1,7 +1,77 @@
+import { useState } from 'react'
 
+export default function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const loginForm = document.forms.loginForm
+  const formData = new FormData(loginForm)
 
-export default function Login () {
-    return (
-        <div></div>
-    )
+  const emailField = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const passwordField = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    try {
+      const response = await fetch('http://localhost:3003/auth/login', {
+        method: 'POST', // POST request
+        headers: {
+          'Content-Type': 'application/json', // Specify JSON format
+        },
+        body: JSON.stringify(Object.fromEntries(formData)), // Convert data to JSON string
+      })
+
+      const result = await response // Parse JSON response from server
+      console.log('Success:', result)
+    } catch (error) {
+      console.error('Error:', error)
+    }
+
+    setEmail('')
+    setPassword('')
+  }
+
+  return (
+    <div className="login">
+      <div className="login-card">
+        <form id="login-form" onSubmit={handleSubmit}>
+          <div className="inputs">
+            <h2>Login</h2>
+            <label for="email">Email</label>
+            <input
+              className="email"
+              type="email"
+              onChange={emailField}
+              id="email"
+              value={email}
+              name="email"
+              required
+              minLength="5"
+              maxLength="20"
+              size="22"
+            />
+            <label for="password">Password</label>
+            <input
+              className="password"
+              onChange={passwordField}
+              value={password}
+              type="password"
+              id="password"
+              name="password"
+              required
+              minLength="5"
+              maxLength="20"
+              size="22"
+            />
+            <input type="submit" className="submit" value="I'm Back" />
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
