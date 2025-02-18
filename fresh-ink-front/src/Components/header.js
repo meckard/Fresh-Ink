cfe950cd57a596e84ce58831f712933b0bdcc23e
Micah Eckard <lumberjacksoul339@gmail.com>
2025-02-17 21:22:45 -0500

    added cart icon and count to nav

--- fresh-ink-front/src/Components/header.js
+++ fresh-ink-front/src/Components/header.js
@@ -1,9 +1,12 @@
 import { Link } from 'react-router-dom'
 import { useContext } from 'react'
 import { AuthContext } from './authContext'
+import { CartContext } from './cartContext'
+import cartIcon from '../Assets/cartIcon.svg'
 
 const Header = () => {
   const { user, setUser } = useContext(AuthContext)
+  const { cart } = useContext(CartContext)
   const logout = async () => {
     const response = await fetch('https://localhost:3003/auth/logout', {
       method: 'POST',
@@ -20,10 +23,10 @@ const Header = () => {
       <nav>
         <ul className="link-list">
           <li className="link">
-            <Link to='/'>Home</Link>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to='/products'>Products</Link>
+            <Link to="/products">Products</Link>
           </li>
           <li>
             <Link>About Us</Link>
@@ -31,8 +34,29 @@ const Header = () => {
         </ul>
         <div className="nav-user">
           <div>
-            { user ? `hello ${user.email}`: <Link to='/login'>Login</Link>  }
-            { user ? <button onClick={logout}>logout</button> : ''}
+            {user ? (
+              <Link to="/profile">Profile</Link>
+            ) : (
+              <Link to="/login">Login</Link>
+            )}
+            {user ? (
+              <button className="nav-logout" onClick={logout}>
+                Logout
+              </button>
+            ) : (
+              ''
+            )}
+          </div>
+          <div className="nav-cart">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path
+                d="M351.9 329.506H206.81l-3.072-12.56H368.16l26.63-116.019-217.23-26.04-9.952-58.09h-50.4v21.946h31.894l35.233 191.246a32.927 32.927 0 1 0 36.363 21.462h100.244a32.825 32.825 0 1 0 30.957-21.945zM181.427 197.45l186.51 22.358-17.258 75.195H198.917z"
+                data-name="Shopping Cart"
+              />
+            </svg>
+            <div className="nav-cart-number">
+              <p>{cart.itemsInCart}</p>
+            </div>
           </div>
         </div>
       </nav>
